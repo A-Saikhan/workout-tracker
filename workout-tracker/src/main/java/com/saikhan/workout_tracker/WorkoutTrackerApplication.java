eeefeb51bc3f7e1545bfd33a567586c27468d7a2
Saikhan <saykhanabu123@gmail.com>
2026-07-19 14:45:38 +0200

    initial commit

---
+++ workout-tracker/src/main/java/com/saikhan/workout_tracker/WorkoutTrackerApplication.java
@@ -0,0 +1,19 @@
+package com.saikhan.workout_tracker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WorkoutTrackerApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(WorkoutTrackerApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutTrackerApplication.class, args);
+	}
+
+}
