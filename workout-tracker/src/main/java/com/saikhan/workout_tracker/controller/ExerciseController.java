e7e7dde3be5ccc4a75ae4e4a1bd3191cff616ae5
Saikhan <saykhanabu123@gmail.com>
2026-09-02 19:31:24 +0200

    Cors Konfiguration zentral statt per Annotation
    Erlaubte Origins kommen jetzt aus der application.properties und
    gelten für alle Controller.

--- workout-tracker/src/main/java/com/saikhan/workout_tracker/controller/ExerciseController.java
+++ workout-tracker/src/main/java/com/saikhan/workout_tracker/controller/ExerciseController.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/exercises")
 
