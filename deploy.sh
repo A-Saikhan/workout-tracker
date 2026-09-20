531a7b059088d24dfb1b83502901869b7c14ae3b
Saikhan <saykhanabu123@gmail.com>
2026-09-20 21:06:09 +0200

    chore: Zeilenenden in .gitattributes festlegen

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd /home/saikhan/workout-tracker/workout-tracker
+
+docker compose -f docker-compose.yml -f docker-compose.prod.yml pull backend
+
+docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --no-build
+
+for i in {1..30}; do
+  echo "Testing api..."
+  if curl -f -s -o /dev/null "localhost:8080/api/exercises?size=1"; then
+    echo "API is running."
+    exit 0
+  else
+    echo "Failed: Trying again in 5 seconds."
+    sleep 5
+  fi
+done
+
+exit 1
